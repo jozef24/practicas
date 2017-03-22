ce4940d287a4ad9ba43eac06f6b7d3190f44e190
jozef <jozef.valverde@easytaxi.com.pe>
2017-03-22 22:52:58 +0000

    creacion del bundle user

--- src/JVL/UserBundle/Controller/UserController.php
+++ src/JVL/UserBundle/Controller/UserController.php
@@ -9,10 +9,25 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        return  new Response('Bienvenido al modulo de usuarios'); 
+        $em = $this->getDoctrine()->getManager();
+        
+        $users = $em->getRepository('JVLUserBundle:User')->findAll();
+        
+        /*$res = 'lusta de ususarios<br/>';
+        foreach($users as $user){
+            
+            $res .= 'Usuario: '.$user->getUsername(). ' email'.$user->getEmail().'<br/>';
+        }
+        
+        return new Response($res);*/
+        return $this->render('JVLUserBundle:User:index.html.twig',array('users'=>$users));
     }
     
-    public function articlesAction($page){
-        return new Response('este es mi articulo '.$page);
+    public function viewAction($id){
+        $repository = $this->getDoctrine()->getRepository('JVLUserBundle:User');
+        $user = $repository->find($id);
+        return new Response($user->getUsername());
     }
+    
+  
 }
